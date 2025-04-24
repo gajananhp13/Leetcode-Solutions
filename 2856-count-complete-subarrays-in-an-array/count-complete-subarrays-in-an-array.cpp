dcac2a25be9ad58b12428d9af034f06ba49c0f96
gajanan patange <123582811+gajananhp13@users.noreply.github.com>
2025-04-24 12:52:00 +0530

    Time: 1508 ms (14.72%) | Memory: 483.4 MB (11.78%) - LeetSync

---
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        int n=nums.size();
+        unordered_set<int> freq;
+        for(int i=0;i<n;i++){
+            freq.insert(nums[i]);
+            }
+        int setsize=freq.size();
+        int ans=0;
+      
+        for(int i=0;i<n;i++)
+        {  unordered_set<int> temp;
+
+            for(int j=i;j<n;j++)
+            {
+                temp.insert(nums[j]);
+                if(temp.size()==setsize)
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
