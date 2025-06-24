b952e83d63b130e83aff0d2f4e08ac3477587202
gajanan patange <123582811+gajananhp13@users.noreply.github.com>
2025-06-24 23:31:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (77.73%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+char findTheDifference(string s, string t) {
+        int n = s.length(), res = t[n];
+        for(int i = 0; i < n; ++i){
+            res -= s[i];
+            res += t[i];
+        }
+        return res;
+    }
+};
