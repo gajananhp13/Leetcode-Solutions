dacee641feded5b9a86d409632acb2ea0d9a4a2d
gajanan patange <123582811+gajananhp13@users.noreply.github.com>
2025-09-01 18:14:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (62.68%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string ans = "";
+        for(auto x : s){
+            if(x == 'i')
+            reverse(ans.begin(),ans.end());
+            else
+            ans += x;
+        }
+        return ans;
+    }
+};
